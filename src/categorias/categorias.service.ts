9b564b9a7b24560529454b39bc4a06b43f80da14
KaikOliveira <kaik.oliveiraa76@gmail.com>
2022-01-13 22:33:38 -0300

    new module, controller and service categoria

---
+++ src/categorias/categorias.service.ts
@@ -0,0 +1,28 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
+import { Categoria } from './interfaces/categoria.interface';
+
+@Injectable()
+export class CategoriasService {
+  constructor(
+    @InjectModel('Categoria') private readonly categoriaModel: Model<Categoria>,
+  ) {}
+
+  async criarCategoria(criarCategoria: CriarCategoriaDto): Promise<Categoria> {
+    const { categoria } = criarCategoria;
+
+    const categoriaEncontrada = this.categoriaModel
+      .findOne({ categoria })
+      .exec();
+
+    if (categoriaEncontrada) {
+      throw new BadRequestException(`Categoria ${categoria} já cadastrada!`);
+    }
+
+    const categoriaCriada = new this.categoriaModel(CriarCategoriaDto);
+
+    return await categoriaCriada.save();
+  }
+}
